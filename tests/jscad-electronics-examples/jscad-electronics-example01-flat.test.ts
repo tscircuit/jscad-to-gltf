7a964cbcbe08dc6fc23b2984e527aa0f46fbf893
Mustafa Mulla <97171641+MustafaMulla29@users.noreply.github.com>
2025-10-12 14:16:19 +0000

    format fix

--- tests/jscad-electronics-examples/jscad-electronics-example01-flat.test.ts
+++ tests/jscad-electronics-examples/jscad-electronics-example01-flat.test.ts
@@ -3,8 +3,7 @@ import { getJscadModelForFootprint } from "jscad-electronics/vanilla"
 import { renderGLTFToPNGBufferFromGLBBuffer } from "poppygl"
 import * as jscadModeling from "@jscad/modeling"
 
-import { 
-  convertJscadModelToGltf } from "../../lib/index"
+import { convertJscadModelToGltf } from "../../lib/index"
 
 test("jscad-electronics-example01-flat (with axis transformation)", async () => {
   const model = getJscadModelForFootprint("soic8", jscadModeling as any)
